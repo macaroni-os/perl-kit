macaroni-os/perl-kit

commit 11897d4e01fe368ed644a82ef245cc387dc2980b

dev-perl/FIle-Next - update to latest upstream(v1.18) dev-perl/IO-Tty - update to latest upstream(v1.14)
NOTE: packages are affected by FL-6941(Apply EAPI 7 to select perl eclasses)
Resolving bug report will allow packages to be successfully installed.

Both packages are dependencies to sys-apps/ack(v3.3.1).

dev-perl/File-Next/File-Next-1.180.0.ebuild
@@ -1,0 +1,29 @@
+# Copyright 1999-2019 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DIST_AUTHOR=PETDANCE
+DIST_VERSION=1.18
+inherit perl-module
+
+DESCRIPTION="File::Next is an iterator-based module for finding files"
+
+LICENSE="Artistic-2"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ppc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~x86-solaris"
+IUSE="test"
+
+RDEPEND="virtual/perl-File-Spec"
+DEPEND="${RDEPEND}
+	virtual/perl-ExtUtils-MakeMaker
+	test? ( virtual/perl-Test-Simple )
+"
+RESTRICT="!test? ( test )"
+
+src_test() {
+	# Ugh, Upstream has tests that depend on tests ...
+	echo 'print qq[1..1\nok 1];' > "${S}/t/pod.t"
+	echo 'print qq[1..1\nok 1];' > "${S}/t/pod-coverage.t"
+	perl-module_src_test
+}

dev-perl/File-Next/Manifest
@@ -1,1 +1,2 @@
 DIST File-Next-1.16.tar.gz 17914 SHA256 6965f25c2c132d0ba7a6f72b57b8bc6d25cf8c1b7032caa3a9bda8612e41d759 SHA512 42c812b8927a18e1f01758a2218f37f2079083117763b41adcee1eb4947b36b1379405707304b948fddd4640e2c34070f4dcd0f217fb60b7ba765efe98be8ad3 WHIRLPOOL 5dcb5994a79cf9aec8aaec143f7ffdb6c87f3de1b52a1be15872e653c89ec7df8e58ea38e7b664e696105d99bee9c28acaf917e33118277bf3bfb8163feb6ee1
+DIST File-Next-1.18.tar.gz 18286 BLAKE2B bac27a198bb8c8fa887b09fcc9f7473191e236da62097fd93759b34c436676402da3e65c89e18eb30ffc6dae0fda3bc7b4b750c5483442af345b8e89ee3760d7 SHA512 951ad995b40ddda359a10ba2d7e19622059da03e7aaa3411e2e887633fbf7720554dc3f98ad581b245b2ffb51ea79f7c5eedecf6e943359eb407df0f9014e627

dev-perl/IO-Tty/IO-Tty-1.140.0.ebuild
@@ -1,0 +1,22 @@
+# Copyright 1999-2015 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DIST_AUTHOR=TODDR
+DIST_VERSION=1.14
+inherit perl-module
+
+DESCRIPTION="IO::Tty and IO::Pty modules for Perl"
+
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ppc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~x86-solaris"
+IUSE=""
+
+DEPEND="${RDEPEND}
+	virtual/perl-ExtUtils-MakeMaker
+"
+
+src_test() {
+	perl-module_src_test
+}

dev-perl/IO-Tty/Manifest
@@ -1,1 +1,1 @@
-DIST IO-Tty-1.12.tar.gz 24564 SHA256 a2ef8770d3309178203f8c8ac25e623e63cf76e97830fd3be280ade1a555290d SHA512 23733e7832a0de370952420df8a781f85d28ff60eb4cd55be22147337bf5587f4e1ec7fcfc190ae3ad1fd9df0f9697f2d647e00739f4a2927b1ac9c81435454d WHIRLPOOL 8647109386ce96ec20c7236b7cd2cde40a553e16a3f4bf009de5718c1e8288adb82a6b0a8755c67fbbeba20558e5bba1bdda07969bf7e47e7aa322550ef1a75f
+DIST IO-Tty-1.14.tar.gz 27756 BLAKE2B 595a2a52d788981cd3235f9609936279291d362674785b2e798b3cdb58e1733b5dfdcc0672070da0bccdeac8565cd90461db7cdd8ead138c4e3b510f5e9d52b0 SHA512 4d6a5299f87eab0281f5e26953bc1537d760d88c486de9c8512750af733d5e466e059400a310746eb8fe432b8c8dcb82055c4cfbb1d31e25c9ce874557618305

commit b7e75fdfe166bc300467f6e0fd14f7d0f1ae623d

Update perl dependencies to sys-apps/ack
dev-perl/FIle-Next - update to latest upstream(v1.18)
dev-perl/IO-Tty - update to latest upstream(v1.14)

NOTE: packages are affected by FL-6941(Apply EAPI 7 to select perl eclasses)
Resolving bug report will allow packages to be successfully installed.

Both packages are dependencies to sys-apps/ack(v3.3.1).

EDIT: cherry pick commit 11897d4e01fe368ed644a82ef245cc387dc2980b
mistakenly committed to master branch and apply to 5.28-release.

Commit Resolves FL-7020.

dev-perl/File-Next/File-Next-1.180.0.ebuild
@@ -1,0 +1,29 @@
+# Copyright 1999-2019 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DIST_AUTHOR=PETDANCE
+DIST_VERSION=1.18
+inherit perl-module
+
+DESCRIPTION="File::Next is an iterator-based module for finding files"
+
+LICENSE="Artistic-2"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ppc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~x86-solaris"
+IUSE="test"
+
+RDEPEND="virtual/perl-File-Spec"
+DEPEND="${RDEPEND}
+	virtual/perl-ExtUtils-MakeMaker
+	test? ( virtual/perl-Test-Simple )
+"
+RESTRICT="!test? ( test )"
+
+src_test() {
+	# Ugh, Upstream has tests that depend on tests ...
+	echo 'print qq[1..1\nok 1];' > "${S}/t/pod.t"
+	echo 'print qq[1..1\nok 1];' > "${S}/t/pod-coverage.t"
+	perl-module_src_test
+}

dev-perl/File-Next/Manifest
@@ -1,1 +1,2 @@
-DIST File-Next-1.16.tar.gz 17914 BLAKE2B 36e12dbc05830b622d4b77db803b63e2b1ef679a346634f13b40ab8d6b4a3f5ff2a43b6015930bc7e3fc3cd29829b8a0d76ce7ef2f3e1584699a9c70a23fd48d SHA512 42c812b8927a18e1f01758a2218f37f2079083117763b41adcee1eb4947b36b1379405707304b948fddd4640e2c34070f4dcd0f217fb60b7ba765efe98be8ad3
+DIST File-Next-1.16.tar.gz 17914 SHA256 6965f25c2c132d0ba7a6f72b57b8bc6d25cf8c1b7032caa3a9bda8612e41d759 SHA512 42c812b8927a18e1f01758a2218f37f2079083117763b41adcee1eb4947b36b1379405707304b948fddd4640e2c34070f4dcd0f217fb60b7ba765efe98be8ad3 WHIRLPOOL 5dcb5994a79cf9aec8aaec143f7ffdb6c87f3de1b52a1be15872e653c89ec7df8e58ea38e7b664e696105d99bee9c28acaf917e33118277bf3bfb8163feb6ee1
+DIST File-Next-1.18.tar.gz 18286 BLAKE2B bac27a198bb8c8fa887b09fcc9f7473191e236da62097fd93759b34c436676402da3e65c89e18eb30ffc6dae0fda3bc7b4b750c5483442af345b8e89ee3760d7 SHA512 951ad995b40ddda359a10ba2d7e19622059da03e7aaa3411e2e887633fbf7720554dc3f98ad581b245b2ffb51ea79f7c5eedecf6e943359eb407df0f9014e627

dev-perl/IO-Tty/IO-Tty-1.140.0.ebuild
@@ -1,0 +1,22 @@
+# Copyright 1999-2015 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DIST_AUTHOR=TODDR
+DIST_VERSION=1.14
+inherit perl-module
+
+DESCRIPTION="IO::Tty and IO::Pty modules for Perl"
+
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ppc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~x86-solaris"
+IUSE=""
+
+DEPEND="${RDEPEND}
+	virtual/perl-ExtUtils-MakeMaker
+"
+
+src_test() {
+	perl-module_src_test
+}

dev-perl/IO-Tty/Manifest
@@ -1,1 +1,1 @@
-DIST IO-Tty-1.12.tar.gz 24564 BLAKE2B da0b252d938b115e8cb7e46866829fcf76a56754c5c3cde8f64dcddda4c9bde9cb77a1b1a022dc8ff94f785d1e1020f76be3ff805c50f276902128d397039244 SHA512 23733e7832a0de370952420df8a781f85d28ff60eb4cd55be22147337bf5587f4e1ec7fcfc190ae3ad1fd9df0f9697f2d647e00739f4a2927b1ac9c81435454d
+DIST IO-Tty-1.14.tar.gz 27756 BLAKE2B 595a2a52d788981cd3235f9609936279291d362674785b2e798b3cdb58e1733b5dfdcc0672070da0bccdeac8565cd90461db7cdd8ead138c4e3b510f5e9d52b0 SHA512 4d6a5299f87eab0281f5e26953bc1537d760d88c486de9c8512750af733d5e466e059400a310746eb8fe432b8c8dcb82055c4cfbb1d31e25c9ce874557618305