07a6cc830de5d563c9a70e97ff9e92a6eb109015
Daniel Robbins <drobbins@funtoo.org>
2022-03-31 18:42:09 -0600

    Autogenerated tree updates.
    merged:
    
      perl-kit-sources: fa29d0130cb74f4b6583086cbe0ab32f50d4c599
      kit-fixups: c72ee78406cff102c7cb83b37f2bee908debe710

---
+++ dev-perl/Gtk3/autogen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+from packaging.version import Version
+import re
+
+async def generate(hub, **pkginfo):
+	homepage = "https://metacpan.org/pod/Gtk3"
+	regex = r'(\d+(?:\.\d+)+)'
+	html = await hub.pkgtools.fetch.get_page(homepage)
+	soup = BeautifulSoup(html, "html.parser").find("li", class_="version-jump").find_all("option")
+
+	options = [opt.get('value').split('/') for opt in soup if opt and opt.get('value')]
+
+	versions = [(re.findall(regex, a[1])[0], a[0]) for a in options]
+	latest = max([(Version(v[0]), v[0], v[1]) for v in versions])
+
+	links = BeautifulSoup(html, "html.parser").find_all("a")
+	download_url = [a.get('href') for a in links if a.get('href') and '.tar.' in a.get('href')][0]
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version = latest[0],
+		dist_version = latest[1],
+		dist_author = latest[2],
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=download_url)]
+	)
+	ebuild.push()
+
+# vim: ts=4 sw=4 noet
